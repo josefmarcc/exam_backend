ee5c8507f53fc5de441959f67ea533f24f322e63
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2021-01-23 12:22:28 +0100

    Added BookingResource to seperate

--- src/main/java/rest/BookingResource.java
+++ src/main/java/rest/BookingResource.java
@@ -1,17 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package rest;
 
 import DTO.BookingDTO;
-import DTO.HotelDTO;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.nimbusds.jose.shaded.json.parser.ParseException;
 import errorhandling.NotFoundException;
 import facades.BookingFacade;
-import facades.HotelFacade;
-import java.io.IOException;
 import java.util.List;
-import javax.annotation.security.RolesAllowed;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -23,20 +22,17 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
-
 import utils.EMF_Creator;
-import utils.DataFetcher;
 
 /**
  *
  * @author Josef Marc Pedersen <cph-jp325@cphbusiness.dk>
  */
-@Path("hotels")
-public class HotelResource {
+@Path("booking")
+public class BookingResource {
 
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
-    private static final HotelFacade hf = HotelFacade.getHotelFacade(EMF);
     private static final BookingFacade bf = BookingFacade.getBookingFacade(EMF);
 
     @Context
@@ -45,18 +41,22 @@ public class HotelResource {
     @Context
     SecurityContext securityContext;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String allHotels() throws IOException, InterruptedException {
-        return DataFetcher.fetchFromHotelsApi();
+    @POST
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    public String addBooking(String booking) throws NotFoundException {
+        BookingDTO bookingDTO = gson.fromJson(booking, BookingDTO.class);
+        bf.addBooking(bookingDTO.getStartDate(), bookingDTO.getDays(), bookingDTO.getPrice(), bookingDTO.getHotel(), bookingDTO.getUserName());
+        return gson.toJson(bookingDTO);
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{id}")
-    public String getHotel(@PathParam("id") int id) throws IOException, InterruptedException, ParseException {
-        HotelDTO hotelDTO = DataFetcher.fetchSingleHotel(id);
-        return gson.toJson(hotelDTO);
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Path("/{userName}")
+    public String getUserBookings(@PathParam("userName") String userName) {
+        List<BookingDTO> bookingList = bf.getUserBookings(userName);
+        return gson.toJson(bookingList);
     }
 
 }

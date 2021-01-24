77f90b5842cdc20893ca9d8a56e85277799b051a
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2021-01-24 19:14:12 +0100

    Update HotelResource.java

--- src/main/java/rest/HotelResource.java
+++ src/main/java/rest/HotelResource.java
@@ -1,27 +1,17 @@
 package rest;
 
-import DTO.BookingDTO;
 import DTO.HotelDTO;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.nimbusds.jose.shaded.json.parser.ParseException;
-import errorhandling.NotFoundException;
-import facades.BookingFacade;
-import facades.HotelFacade;
 import java.io.IOException;
-import java.util.List;
-import javax.annotation.security.RolesAllowed;
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
 
 import utils.EMF_Creator;
@@ -36,14 +26,9 @@ public class HotelResource {
 
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
-    private static final HotelFacade hf = HotelFacade.getHotelFacade(EMF);
-    private static final BookingFacade bf = BookingFacade.getBookingFacade(EMF);
 
     @Context
     private UriInfo context;
-
-    @Context
-    SecurityContext securityContext;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
